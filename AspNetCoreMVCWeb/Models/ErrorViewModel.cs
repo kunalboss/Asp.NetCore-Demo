3af986de425acadbcb7dd086453fc2e53d68f4b9
Kunal Jhaveri <kunalboss@gmail.com>
2019-01-01 16:08:25 +0530

    Add files via upload

---
+++ AspNetCoreMVCWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreMVCWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
